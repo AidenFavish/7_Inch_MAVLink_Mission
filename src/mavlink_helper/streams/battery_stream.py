9728b43b9719f82016fc3ccc7a6d247c90dd4d34
Aiden Favish <aidenfavish@gmail.com>
2025-03-26 11:20:17 -0700

    Added logging and docstrings.

--- src/mavlink_helper/streams/battery_stream.py
+++ src/mavlink_helper/streams/battery_stream.py
@@ -4,15 +4,17 @@ from mavlink_helper.streams import Stream, StreamType
 
 class BatteryStream(Stream):
     def __init__(self, time_interval, debug=False):
+        """Get battery status at time interval in microseconds."""
         super().__init__(debug, StreamType.BATTERY_STATUS, time_interval)
         self.latest_msg = None
     
     def update(self, connection: utility.mavserial | utility.mavudp) -> None:
+        self.log("Checking for new battery status...")
         msg = connection.recv_match(type=dialect.MAVLink_battery_status_message.msgname)
         if msg is not None:
+            self.log(f"New battery status found: {msg}")
             self.latest_msg = msg
             self.reset_time()
-            self.log(f"msg: {msg}")
 
     def get_batt_remaining(self) -> int:
         """Percent of battery remaining."""
